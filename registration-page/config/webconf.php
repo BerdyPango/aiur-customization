b5d69def36efdcb58b80a99e844d642553b1841d
FrostHe <1035366983@qq.com>
2019-01-01 20:19:56 +0800

    fix wording

--- registration-page/config/webconf.php
+++ registration-page/config/webconf.php
@@ -1,5 +1,5 @@
 <?php
-$pagetitle = "魔兽世界 80 级公益服「艾尔」";
-$servertitle = "Airu"; //Server title will be displayed on the tab and on the navbar
+$pagetitle = "魔兽世界 80 级公益服";
+$servertitle = "Aiur"; //Server title will be displayed on the tab and on the navbar
 $realmlist = "aiur.frosthe.net"; //realmlist. Do NOT include "set realmlist"
 ?>
--- registration-page/pages/downloads.php
+++ registration-page/pages/downloads.php
@@ -5,11 +5,11 @@
         <div class="panel panel-default">
           <div class="panel-heading">下载</div>
           <div class="panel-body">
-            <h3>魔兽世界 3.3.5a 客户端</h3><br><br>
+            <h3>魔兽世界 3.3.5a 客户端</h3><br>
             <a href="https://pan.baidu.com/s/1uNUSCySE4A8dpDyZn8HJVA" target="_blank">百度网盘下载地址</a>  - 提取密码: <font color="red">3yy1</font><br><br>
             <a href="https://cloud.frosthe.net:8443/d/ef4a508f6c2449e1be11/" target="_blank">FrostHub 下载地址</a>  - 密码: <font color="red">12345678</font><br><br>
 
-            <h3>插件</h3><br><br>
+            <h3>插件</h3><br>
             <a href="https://pan.baidu.com/s/1-pziojSXmR3sm4kGhpH4IA" target="_blank">百度网盘下载地址</a>  - 提取密码: <font color="red">x8qb</font><br><br>
             <a href="https://cloud.frosthe.net:8443/d/cacc806c51f3430f8491/" target="_blank">FrostHub 下载地址</a>  - 密码: <font color="red">12345678</font><br><br>
           </div>
