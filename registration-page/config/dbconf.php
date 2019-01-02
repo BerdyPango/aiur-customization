f5f1398593128bbb952cba9cfca547c1624300e0
James Shih <hyjk2000@users.noreply.github.com>
2019-01-02 16:35:19 +0800

    Set UTF-8 charset for DB connection

--- registration-page/config/dbconf.php
+++ registration-page/config/dbconf.php
@@ -3,7 +3,8 @@ $dbhost = "127.0.0.1";
 $dbuser = "trinity";
 $dbpass = "trinity";
 $accdb = "auth";
-$chardb = "characters";
+$chardb = "utf8";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $accdb);
+mysqli_set_charset($conn, $chardb);
 ?>
