bdfc4a1d86c459d939d206f029c613b47aa17855
FrostHe <1035366983@qq.com>
2019-01-03 08:08:17 +0800

    append character-set variable in dbconf.php

--- registration-page/config/dbconf.php
+++ registration-page/config/dbconf.php
@@ -3,8 +3,9 @@ $dbhost = "127.0.0.1";
 $dbuser = "trinity";
 $dbpass = "trinity";
 $accdb = "auth";
-$chardb = "utf8";
+$chardb = "characters";
+$charset = "utf8";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $accdb);
-mysqli_set_charset($conn, $chardb);
+mysqli_set_charset($conn, $charset);
 ?>
